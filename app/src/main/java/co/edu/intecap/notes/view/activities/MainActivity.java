25aa64770f4251d102849588d68062be6686145b
julio.saldana <julio.saldana@avantica.com>
2019-05-02 18:31:58 -0500

    big screens support

--- app/src/main/java/co/edu/intecap/notes/view/activities/MainActivity.java
+++ app/src/main/java/co/edu/intecap/notes/view/activities/MainActivity.java
@@ -1,28 +1,75 @@
 package co.edu.intecap.notes.view.activities;
 
-import androidx.fragment.app.FragmentManager;
 import co.edu.intecap.notes.R;
+import co.edu.intecap.notes.view.fragments.NoteFormFragment;
 import co.edu.intecap.notes.view.fragments.NoteListFragment;
+import co.edu.intecap.notes.view.listeners.NoteEventListener;
+import co.edu.intecap.notes.view.listeners.NoteFormEventListener;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
 
-public class MainActivity extends BaseActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-
+public class MainActivity extends BaseActivity implements NoteEventListener, NoteFormEventListener {
+    FrameLayout noteFormContainer;
+    private FloatingActionButton fabAddNote;
+    private NoteListFragment noteListFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        noteFormContainer = findViewById(R.id.note_form_container);
         setupToolbar();
         setToolbarTitle(R.string.app_name);
         includeListFragment();
+        setupActionButton();
+
+    }
+
+    private void setupActionButton() {
+        fabAddNote = findViewById(R.id.btn_add_note);
+        fabAddNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NoteFormActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     private void includeListFragment() {
+
+        noteListFragment = new NoteListFragment();
         getSupportFragmentManager()
                 .beginTransaction()
-                .add(R.id.note_list_container, new NoteListFragment())
+                .add(R.id.note_list_container, noteListFragment)
                 .commit();
     }
 
+
+    @Override
+    public void onClickNote(int noteId) {
+        if (noteFormContainer == null) {
+            Intent intent = new Intent(MainActivity.this, NoteFormActivity.class);
+            intent.putExtra(NoteFormActivity.EXTRA_NOTE_ID, noteId);
+            startActivity(intent);
+        } else {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.note_form_container, NoteFormFragment.getInstance(noteId))
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public void onAddNote() {
+        if(noteListFragment != null){
+            noteListFragment.update();
+        }
+
+    }
 }
--- app/src/main/java/co/edu/intecap/notes/view/fragments/NoteListFragment.java
+++ app/src/main/java/co/edu/intecap/notes/view/fragments/NoteListFragment.java
@@ -1,6 +1,7 @@
 package co.edu.intecap.notes.view.fragments;
 
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -15,6 +16,7 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import co.edu.intecap.notes.R;
+import co.edu.intecap.notes.model.datamodels.Note;
 import co.edu.intecap.notes.model.repositories.NotesRepository;
 import co.edu.intecap.notes.view.activities.MainActivity;
 import co.edu.intecap.notes.view.activities.NoteFormActivity;
@@ -24,11 +26,19 @@ import co.edu.intecap.notes.view.listeners.NoteEventListener;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class NoteListFragment extends Fragment  implements NoteEventListener {
+public class NoteListFragment extends Fragment{
 
     private RecyclerView rvNotes;
-    private FloatingActionButton fabAddNote;
     private NotesAdapter adapter;
+    private NoteEventListener noteEventListener;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if(context instanceof  NoteEventListener){
+            noteEventListener = (NoteEventListener) context;
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -36,19 +46,10 @@ public class NoteListFragment extends Fragment  implements NoteEventListener {
         View view = inflater.inflate(R.layout.fragment_note_list, container, false);
 
         rvNotes = view.findViewById(R.id.rv_notes);
-        fabAddNote = view.findViewById(R.id.btn_add_note);
-        fabAddNote.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getContext(), NoteFormActivity.class);
-                startActivity(intent);
-            }
-        });
         setupNoteList();
 
         return view;
     }
-
 
     @Override
     public void onResume() {
@@ -58,17 +59,12 @@ public class NoteListFragment extends Fragment  implements NoteEventListener {
 
     private void setupNoteList() {
         rvNotes.setLayoutManager(new LinearLayoutManager(getContext()));
-        adapter =  new NotesAdapter(this);
+        adapter =  new NotesAdapter(noteEventListener);
         adapter.setNoteList(NotesRepository.NOTES_LIST);
         rvNotes.setAdapter(adapter);
     }
 
-    @Override
-    public void onClickNote(int noteId) {
-        Intent intent = new Intent(getContext(), NoteFormActivity.class);
-        intent.putExtra(NoteFormActivity.EXTRA_NOTE_ID, noteId);
-        startActivity(intent);
-
+    public void update() {
+        adapter.notifyDataSetChanged();
     }
-
 }
